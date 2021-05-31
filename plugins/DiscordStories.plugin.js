a7eea2e3ee6f80a03c3f3a9f6481f68fc59ee21e
Pickaxe828 <joshuajcn828@gmail.com>
2021-05-31 22:40:11 +0800

    Update DiscordStories.plugin.js

--- plugins/DiscordStories.plugin.js
+++ plugins/DiscordStories.plugin.js
@@ -4,7 +4,9 @@
  * @version _Development
  * @invite nKbSGzW6
  * @authorId 465026906450821121
-**/
+ */
+
+// @updateUrl https://raw.githubusercontent.com/pickaxe828/Discord-Stories/main/discord_stories.plugin.js
 
 let plugin_name = "DiscordStories";
 
@@ -29,57 +31,23 @@ module.exports = class DiscordStories {
         return "Pickaxe828#4395";
     }
 
-    load() { } // Optional function. Called when the plugin is loaded in to memory
+    load() { 
+        updater()
+    }
+
+    unload() { }
 
     start() {
-        if (BdApi.Themes.isEnabled("DiscordStories")) { } else {
-            this.startUpFatal();
-        }
-        
+        init()
+        frdListUpload()
+
     }
 
     stop() { } // Required function. Called when the plugin is deactivated
 
-    getAddPanel() { }
-    
-    getSettingsPanel() {
-
-    }
-
-    dummyIconGen() {
-
-    }
-
-    
-
-    startUpFatal() {
-        //BdApi.showConfirmationModal("Discord Stories",
-        //    [
-        //        "Theme need to be loaded first! Download it ",
-        //        BdApi.React.createElement("a", { href: "https://google.com", target: "_blank" }, "here")
-        //    ],
-        //    {
-        //        confirmText: "Okay",
-        //        cancelText: false
-        //    }
-        //).then(
-        //    BdApi.Plugins.disable(plugin_name));
-        
-    }
-
-
-    UIrefresh() {
-        BdApi.reactDOM.render(<h1>Hello, world!</h1>, document.getElementsByClassName("container-lRFx4q")[0])
-    }
-
-    
-
     observer(changes) {
-        if (document.location["href"] === "https://discord.com/channels/@me") {
-            console.log("observed");
-            this.UIrefresh();
-        }
-        // Optional function. Observer for the `document`. Better documentation than I can provide is found here: <https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver>
+        uiRefresh()
+
     }
 
 }
