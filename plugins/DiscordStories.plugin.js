11eba9eda31f3ac22a2e9ff0d306c22a35a34d3a
Pickaxe828 <joshuajcn828@gmail.com>
2021-06-01 21:21:55 +0800

    Update DiscordStories.plugin.js

--- plugins/DiscordStories.plugin.js
+++ plugins/DiscordStories.plugin.js
@@ -1,53 +1,44 @@
 /**
  * @name DiscordStories
+ * @author Pickaxe828#4395
  * @description Let you be able to post and see stories like how Instagram stories works
  * @version _Development
  * @invite nKbSGzW6
- * @authorId 465026906450821121
- */
-
-// @updateUrl https://raw.githubusercontent.com/pickaxe828/Discord-Stories/main/discord_stories.plugin.js
-
-let plugin_name = "DiscordStories";
+ * @updateUrl https://raw.githubusercontent.com/pickaxe828/Discord-Stories/main/discord_stories.plugin.js
+ **/
 
 
 module.exports = class DiscordStories {
 
-    TODO() { }
-
-    getName() {
-        return "DiscordStories";
-    }
-
-    getDescription() {
-        return 'Discord Stories third party plug-in!';
-    }
-
-    getVersion() {
-        return "_internalTest";
-    }
-
-    getAuthor() {
-        return "Pickaxe828#4395";
-    }
-
     load() { 
-        updater()
+        
     }
 
     unload() { }
 
     start() {
-        init()
-        frdListUpload()
+        this.updater()
+        this.init()
+        this.frdListUpload()
 
     }
 
     stop() { } // Required function. Called when the plugin is deactivated
 
     observer(changes) {
-        uiRefresh()
+        this.uiRefresh()
 
     }
 
+    updater() {
+        const fs = require("fs");
+        //%appdata%\betterdiscord\plugins
+        let request = new XMLHttpRequest();
+        request.open("https://api.github.com/projects/349918168", {headers:{"Accept":"application/vnd.github.v3+json"}});
+        console.log(request);
+    }
+
+    init() {}
+    frdListUpload() {}
+    uiRefresh() {}
 }
